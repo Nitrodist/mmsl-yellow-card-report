a2e5b502be412c9a6adad69b5d8d3a3e5ba4f937
Mark Campbell <nitrodist@gmail.com>
2020-02-15 23:45:25 -0600

    WIP

--- mmsl_report.sql
+++ mmsl_report.sql
@@ -1,2 +1,17 @@
-SELECT team_name, concat(first_name, ' ', last_name) as name
-FROM infractions;
+SELECT i.team_name, 
+       concat(i.first_name, ' ', i.last_name) as name,
+      infract1_date,
+      infract2_date,
+      infract3_date,
+      infract4_date
+--      infract5_date,
+--      infract6_date,
+--      infract7_date,
+FROM infractions i
+left outer join (select team_name, first_name, last_name, datetime_of_infraction as infract1_date FROM infractions WHERE infraction_series_number = 1) infract1 ON i.team_name = infract1.team_name AND i.first_name = infract1.first_name AND i.last_name = infract1.last_name
+left outer join (select team_name, first_name, last_name, datetime_of_infraction as infract2_date FROM infractions WHERE infraction_series_number = 2) infract2 ON i.team_name = infract2.team_name AND i.first_name = infract2.first_name AND i.last_name = infract2.last_name
+left outer join (select team_name, first_name, last_name, datetime_of_infraction as infract3_date FROM infractions WHERE infraction_series_number = 3) infract3 ON i.team_name = infract3.team_name AND i.first_name = infract3.first_name AND i.last_name = infract3.last_name
+left outer join (select team_name, first_name, last_name, datetime_of_infraction as infract4_date FROM infractions WHERE infraction_series_number = 4) infract4 ON i.team_name = infract4.team_name AND i.first_name = infract4.first_name AND i.last_name = infract4.last_name
+
+GROUP BY 1, 2, 3, 4, 5, 6
+;
--- process.rb
+++ process.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-#
+
 require 'csv'
 require 'pry'
 require 'active_record'
@@ -19,6 +19,7 @@ csv_data = CSV.parse(File.read('penalty-report-202002151117.csv'), headers: true
 #     datetime_of_infraction TIMESTAMPTZ,
 #     player_number INTEGER,
 #     infraction_code varchar(255),
+#     infraction_series_number INTEGER,
 #     PRIMARY KEY(id)
 #   )
 # EOS
@@ -26,18 +27,29 @@ csv_data = CSV.parse(File.read('penalty-report-202002151117.csv'), headers: true
 class Infraction < ActiveRecord::Base
 end
 
+Infraction.delete_all
+
 csv_data.each do |row|
-  Infraction.create(
+  next if row[' Card'] != 'Yellow Card'
+  record = {
     id: Infraction.count + 1,
     last_name: row[' Last Name'].strip,
     first_name: row['First Name'].strip,
     team_name: row['Team'].strip,
     player_number: row['#'].strip,
     infraction_code: row[' Card'].strip,
-    datetime_of_infraction: DateTime.parse(row['Date'].strip + " Central Time (US & Canada)"))
+    datetime_of_infraction: DateTime.parse(row['Date'].strip + " Central Time (US & Canada)"),
+  }
+
+  infraction_series_number = Infraction.where(last_name: record[:last_name], first_name: record[:first_name], team_name: record[:team_name]).count + 1
+
+  record = Infraction.create(record.merge(infraction_series_number: infraction_series_number))
+
+end
+
+def generate_infraction_series_number
 end
 
 binding.pry
-
 
 exit 0
